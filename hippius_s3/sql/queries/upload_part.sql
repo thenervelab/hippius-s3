87c43c62ed82f7c71cb6d5cda6aa6150d58a4f99
radu.mutilica <radu.mutilica@blackcurve.com>
2025-08-26 00:53:08 +0300

    Fixed multipart uploads/downloads

--- hippius_s3/sql/queries/upload_part.sql
+++ hippius_s3/sql/queries/upload_part.sql
@@ -4,4 +4,9 @@ INSERT INTO parts (
     part_id, upload_id, part_number, ipfs_cid, size_bytes, etag, uploaded_at
 )
 VALUES ($1, $2, $3, $4, $5, $6, $7)
+ON CONFLICT (upload_id, part_number) DO UPDATE SET
+    ipfs_cid = EXCLUDED.ipfs_cid,
+    size_bytes = EXCLUDED.size_bytes,
+    etag = EXCLUDED.etag,
+    uploaded_at = EXCLUDED.uploaded_at
 RETURNING part_id, upload_id, part_number, etag
