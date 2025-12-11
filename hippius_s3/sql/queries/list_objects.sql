874099a88b710fc1cbadc759264a1990e80d1e3a
radu.mutilica <radu.mutilica@blackcurve.com>
2025-12-11 16:23:15 +0200

    Updated list queries to ignore failed files

--- hippius_s3/sql/queries/list_objects.sql
+++ hippius_s3/sql/queries/list_objects.sql
@@ -10,4 +10,5 @@ JOIN buckets b ON o.bucket_id = b.bucket_id
 LEFT JOIN cids c ON ov.cid_id = c.id
 WHERE o.bucket_id = $1
   AND ($2::text IS NULL OR o.object_key LIKE $2::text || '%')
+  AND ov.status != 'failed'
 ORDER BY o.object_key COLLATE "C"
